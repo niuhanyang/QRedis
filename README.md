5fa34b58f58ba0a7b2c251b3f4de40d8884802fa
牛汉阳 <bjniuhanyang@corp.netease.com>
2016-08-08 14:38:39 +0800

    frist

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # QRedis
-查询redis数据
+这是一个查询redis数据的web客户端，启动之后可以访问web页面，取redis数据
+#依赖
+Python
--- app/src/coon_redis.py
+++ app/src/coon_redis.py
@@ -1,8 +1,6 @@
 import redis
 from app.src.CONST import *
-coon = redis.Redis(host=REDIS_HOST,port=REDIS_PORT,password=REDIS_PASSWD)
 def get_rcoon():
-	# print coon.hgetall('ad.nadp.consume')
-	# print coon.hget('ad.nadp.consume','')
+	coon = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWD)
 	return coon
 get_rcoon()
